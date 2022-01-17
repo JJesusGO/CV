1526eb00eb13a4f736d729a4c335e30d416b7b66
JJesusGO <jdj.gutierrezornelas@hotmail.com>
2022-01-17 15:48:54 -0600

    Agregando Modal

--- Desarrollo/cv-frontend/src/app/App.tsx
+++ Desarrollo/cv-frontend/src/app/App.tsx
@@ -2,6 +2,8 @@ import Boton, { BotonTipo } from '../react-framework/componentes/basicos/Boton';
 import { PaletaColor } from '../react-framework/componentes/types';
 import styled from 'styled-components';
 import Panel, { PanelTipo } from '../react-framework/componentes/basicos/Panel';
+import Modal, { ModalContenido, ModalContenidoTipo, ModalDialog, ModalTipo} from '../react-framework/componentes/basicos/Modal';
+import { useState } from 'react';
 
 const Botonazo = styled(Boton)`
   color:black !important;
@@ -25,6 +27,7 @@ const Panelazo = styled(Panel)`
 `;
 
 function App() {
+  const [enable, setEnable] = useState(true)
   return (
     <div className="App">
       <Panelazo tipo={PanelTipo.FLEX} className="flex-column" breakpoint='sm'>
@@ -43,6 +46,19 @@ function App() {
           Boton
         </Botonazo>
       </Panelazo>            
+      <Modal tipo={ModalTipo.BASICO} enable={enable} onClose={()=>setEnable(false)}>
+        <ModalDialog breakpoint='lg'>
+          <ModalContenido tipo={ModalContenidoTipo.HEADER} className="bkg-blanco">
+            <h1>HEADER</h1>
+          </ModalContenido>          
+          <ModalContenido tipo={ModalContenidoTipo.BODY} className="bkg-blanco">
+            <h1>CONTENIDO</h1>
+          </ModalContenido>          
+          <ModalContenido tipo={ModalContenidoTipo.FOOTER} className="bkg-blanco">
+            <h1>FOOTER</h1>
+          </ModalContenido>          
+        </ModalDialog>
+      </Modal>
     </div>
   );
 }
